465ab53b8d9b8b3dacc68100ef57f96851263d75
wayvBB <gonzales.isabella1414@gmail.com>
2025-03-09 21:45:55 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/act5/',  // Ensure this matches your GitHub Pages subfolder
   plugins: [react()],
-})
+});
